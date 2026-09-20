b1f258907e0b5c7521b88997006af66d2b94a5d6
SalehKar <saleh.kar@outlook.com>
2026-09-20 15:52:12 +0300

    Replace Storage Advisor artwork with approved icon

--- src/components/StorageAdvisorMark.js
+++ src/components/StorageAdvisorMark.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const StorageAdvisorMark = ({ className = 'h-28 w-28' }) => (
   <img
-    src="/images/icons/storage-advisor-icon.png"
+    src="/images/icons/storage-advisor-icon.jpg"
     alt="Storage Advisor by TekFin"
     className={className}
   />
