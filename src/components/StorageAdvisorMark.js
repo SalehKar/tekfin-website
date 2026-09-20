76b91c020f35c02ea0f11d54a1d0370ae74c216d
SalehKar <saleh.kar@outlook.com>
2026-09-20 15:56:48 +0300

    Restore approved high-resolution Storage Advisor PNG

--- src/components/StorageAdvisorMark.js
+++ src/components/StorageAdvisorMark.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const StorageAdvisorMark = ({ className = 'h-28 w-28' }) => (
   <img
-    src="/images/icons/storage-advisor-icon.jpg"
+    src="/images/icons/storage-advisor-icon.png"
     alt="Storage Advisor by TekFin"
     className={className}
   />
